3436488ac23d6dc56688ac3477b2ed3519d9c0a1
Henrique Silveira <henriquemartinsilveira2@gmail.com>
2024-09-11 15:26:53 -0300

    add rest_frameworks in apps

--- jobber/settings.py
+++ jobber/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
     'tasks' # Chamando meu app rest da todolist
 ]
 
